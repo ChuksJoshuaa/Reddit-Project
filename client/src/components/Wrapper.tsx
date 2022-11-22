cef1c8e4a621fe29cbc7569f6357574303b16b46
D-coder <chuksjoshuaa@gmail.com>
2022-11-22 07:59:31 +0100

    Updated Changes

--- client/src/components/Wrapper.tsx
+++ client/src/components/Wrapper.tsx
@@ -3,11 +3,17 @@ import { Box } from "@chakra-ui/react";
 
 interface WrapperProps {
   children: any;
+  variant?: "small" | "regular";
 }
 
-const Wrapper: React.FC<WrapperProps> = ({ children }) => {
+const Wrapper: React.FC<WrapperProps> = ({ children, variant = "regular" }) => {
   return (
-    <Box mt={8} mx="auto" maxW="800px" w="100%">
+    <Box
+      mt={8}
+      mx="auto"
+      maxW={variant === "regular" ? "800px" : "400px"}
+      w="100%"
+    >
       {children}
     </Box>
   );
