a039e12e49d6991742c2c64797b71a0e784a06e9
D-coder <chuksjoshuaa@gmail.com>
2022-11-23 11:12:27 +0100

    WIP: Added Login Page

---
+++ client/src/pages/login.tsx
@@ -0,0 +1,54 @@
+import { Form, Formik } from "formik";
+import { Box, Button } from "@chakra-ui/react";
+import { InputField, Wrapper } from "../components";
+import { useLoginMutation } from "../generated/graphql";
+import { toErrorMap } from "../utils/toErrorMap";
+import { useRouter } from "next/router";
+
+interface IProps {}
+
+const Login: React.FC<IProps> = () => {
+  const [, login] = useLoginMutation();
+  const Router = useRouter();
+
+  return (
+    <Wrapper variant="small">
+      <Formik
+        initialValues={{ username: "", password: "" }}
+        onSubmit={async (values, { setErrors }) => {
+          const response = await login(values);
+
+          if (response.data?.login.errors) {
+            setErrors(toErrorMap(response.data?.login.errors));
+          } else if (response.data?.login.user) {
+            Router.push("/");
+          }
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField name="username" placeholder="name" label="Username" />
+            <Box mt={4}>
+              <InputField
+                name="password"
+                placeholder="password"
+                label="Password"
+                type="password"
+              />
+            </Box>
+            <Button
+              mt={4}
+              type="submit"
+              isLoading={isSubmitting}
+              colorScheme="teal"
+            >
+              register
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
+};
+
+export default Login;
